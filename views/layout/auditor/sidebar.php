dc6f01aa5087a50f320f61cae1a973a794971348
wayosu <wahyusman10@gmail.com>
2021-03-23 19:26:41 +0800

    laporan for input input laporan

--- views/layout/auditor/sidebar.php
+++ views/layout/auditor/sidebar.php
@@ -29,9 +29,9 @@
                 </a>
             </li>
             <li class="nav-item w-100">
-                <a class="nav-link" href="#">
-                    <i class="fe fe-edit fe-16"></i>
-                    <span class="ml-3 item-text">Input Laporan</span>
+                <a class="nav-link" href="laporan">
+                    <i class="fe fe-file-text fe-16"></i>
+                    <span class="ml-3 item-text">Laporan</span>
                 </a>
             </li>
         </ul>
--- views/page/auditor/index.php
+++ views/page/auditor/index.php
@@ -2,6 +2,15 @@
 if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "beranda_auditor") {
     $page = "Beranda";
     $title = $page . " | PO'OPIYOHE";
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "laporan") {
+    $page = "Laporan";
+    $title = $page . " | PO'OPIYOHE";
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "input_laporan") {
+    $page = "Input Laporan";
+    $title = $page . " | PO'OPIYOHE";
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "edit_laporan") {
+    $page = "Edit Laporan";
+    $title = $page . " | PO'OPIYOHE";
 }
 ?>
 
@@ -12,8 +21,13 @@ if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "beranda_auditor"
 <?php
 if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "beranda_auditor") {
     include 'views/page/auditor/beranda.php';
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "laporan") {
+    include 'views/page/auditor/laporan/laporan.php';
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "input_laporan") {
+    include 'views/page/auditor/laporan/input_laporan.php';
+} else if (isset($_GET['views_auditor']) && $_GET['views_auditor'] == "edit_laporan") {
+    include 'views/page/auditor/laporan/edit_laporan.php';
 } else {
-
     include 'views/page/auditor/beranda.php';
 }
 ?>
